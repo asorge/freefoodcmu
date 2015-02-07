3234a99d3c19e098dec141910e03397895ad05f5
bluquar@gmail.com <cmbarker@weh5201-a3.weh.andrew.cmu.edu>
2015-02-07 00:06:35 -0500

    update sdk location..

--- js/freefood.js
+++ js/freefood.js
@@ -29,7 +29,7 @@ $(function() {
 	 var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
 	 if (d.getElementById(id)) {return;}
 	 js = d.createElement('script'); js.id = id; js.async = true;
-	 js.src = "//connect.facebook.net/en_US/sdk.jsxfbml=1&version=v2.0";
+	 js.src = "//connect.facebook.net/en_US/all.js";
 	 ref.parentNode.insertBefore(js, ref);
 	}(document, /*debug*/ false));
 	
