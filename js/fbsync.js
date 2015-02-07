2ac79dc5c0b4bde0638062a0a9fe8b60d3847204
Chris <chrisbarker@cmu.edu>
2015-02-07 17:42:53 -0500

    event saved confirmation

--- js/fbsync.js
+++ js/fbsync.js
@@ -43,6 +43,10 @@ function submitEvent($form) {
 	}, {
 		success: function(object) {
 			console.log("Saved event!");
+			$form.innerHTML("<center><h3>Saved Event!</h3></center>");
+			setTimeout(function() {
+				$form.fadeOut(750, function() { $form.remove(); });
+			}, 1500);
 		},
 		error: function(model, error) {
 			console.log("Failed to save event");
